a8e3e2faf1b5d4e812ddb31e02b853030111ddfc
sauravcell <sauravghosh2304@gmail.com>
2024-11-19 11:20:49 +0530

    updated UI

--- script.js
+++ script.js
@@ -20,11 +20,62 @@ document.addEventListener("DOMContentLoaded", function () {
   const tutorialBtn = document.getElementById('guideButton')
   const chatbody = document.querySelector('.chat-body')
   const footer = document.querySelector('.footer');
+  const close = document.querySelector('.close');
+  const closedBook = document.querySelector(".fa-book ");
+  const openBook = document.querySelector('.fa-book-open');
+  const messagePop = document.getElementById('messagePop');
 
-  // Show chat popup and welcome message on 'Support' button click
+  // (messagePop)?console.log('fx exists') : console.error('no audio')
+
+  openBook.style.display = "none";
+  close.addEventListener("click", () => {
+    supportButton.style.display = "block";
+  })
+
+
+  //replace with text on hover for techSupport button.
+  {
+    // Store the original content of the button
+    const techContent = techButton.innerHTML;
+
+    // Predefined text to show on mouse enter
+    const techText = "Technical Support";
+
+    techButton.addEventListener('mouseenter', () => {
+      techButton.innerHTML = techText;
+    })
+
+
+    // Add mouseleave event listener to restore the original content
+    techButton.addEventListener('mouseleave', function () {
+      techButton.innerHTML = techContent;
+    });
+
+  }
+
+  //replace with text on hover for tutorialSupport button.
+  {
+    const tutorialContent = tutorialBtn.innerHTML;
+
+    tutorialBtn.addEventListener('mouseenter', () => {
+      tutorialBtn.innerHTML = 'SGDMS Guide';
+      closedBook.style.display = "none"
+      openBook.style.display = "none"
+    });
+
+
+    // Add mouseleave event listener to restore the original content
+    tutorialBtn.addEventListener('mouseleave', function () {
+      tutorialBtn.innerHTML = tutorialContent;
+      closedBook.style.display = "block"
+    });
+  }
+
+
   supportButton.onclick = function () {
     chatPopup.classList.toggle('show')
     console.log("hello world")
+    supportButton.style.display = "none";
 
     // welcomeMessageNew.style.display = "block"; // Show welcome message initially
     // questionButtonsDiv.style.display = "none"; // Hide question buttons initially
@@ -33,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   closeModal.onclick = function () {
     chatPopup.classList.toggle('show')
-    answerDisplay.innerHTML = ''; // Clear any previous answers
+    // answerDisplay.innerHTML = ''; // Clear any previous answers
   };// Close chat popup on close button click
 
 
@@ -42,16 +93,31 @@ document.addEventListener("DOMContentLoaded", function () {
     // welcomeMessageNew.style.display = "none"; // Hide welcome message
     // questionButtonsDiv.style.display = "block"; // Show question buttons
     loadQuestions('technicalQuestion'); // Fetch and display technical support questions
+    techButton.classList.add("disabled")
+    setTimeout(() => {
+      techButton.classList.remove("disabled")
+    }, 3000)
   };
 
   tutorialBtn.onclick = function () {
     loadQuestions('tutorialQuestion')
+    tutorialBtn.classList.add("disabled")
+    setTimeout(() => {
+      tutorialBtn.classList.remove("disabled")
+    }, 3000)
   }
+
+
+
+
+
   // Function to fetch and display questions from the database by category
 
   function loadQuestions(category) {
     console.log(category)
-    fetch(`https://chatbot-jcnp.onreder.com/sgdms/${category}`)
+    fetch(`https://chatbot-jcnp.onrender.com/sgdms/${category}`, {
+
+    })
       .then((response) => response.json())
       .then((data) => {
         // Create container for questions
@@ -87,13 +153,13 @@ document.addEventListener("DOMContentLoaded", function () {
             answerContainer.style.width = "300px";
             // Append container to answer chatbody
             chatbody.appendChild(answerContainer);
-            var chatbodyheight = chatBody.offsetHeight; 
-            var footerHeight = footer.offsetHeight; 
+            var chatbodyheight = chatBody.offsetHeight;
+            var footerHeight = footer.offsetHeight;
             var totalHeight = chatbodyheight + footerHeight
             chatBody.scrollTop = (chatBody.scrollHeight - totalHeight) + 100;
-            // techButton.classList.add('disabled')
+            messagePop.play();
           })
-            query.appendChild(li);
+          query.appendChild(li);
 
         });
 
@@ -103,12 +169,12 @@ document.addEventListener("DOMContentLoaded", function () {
         chatbody.appendChild(queryContainer);
         // Scroll the newly added queryContainer into view
         // calculate chatbody view port for scrollig 
-        var chatbodyheight = chatBody.offsetHeight; 
-        var footerHeight = footer.offsetHeight; 
-        var totalHeight = chatbodyheight + footerHeight; 
+        var chatbodyheight = chatBody.offsetHeight;
+        var footerHeight = footer.offsetHeight;
+        var totalHeight = chatbodyheight + footerHeight;
         // queryContainer.scrollIntoView({ behavior: "smooth", block: "end" });
         chatBody.scrollTop = (chatBody.scrollHeight - totalHeight) + 100;
-      
+
       })
       .catch((error) => console.error('Error fetching questions:', error));
   }
